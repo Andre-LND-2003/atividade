202322666c074bdb025cdc12cb8f1f81046266c3
Andre-LND-2003 <84997968+Andre-LND-2003@users.noreply.github.com>
2021-06-18 18:39:13 -0300

    Add files via upload

---
+++ 100_a_1..cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main(){
+	int i=100;
+	int b=1;
+	while(b<=100){
+		cout <<i<<"\n";
+		i--;
+		b++;
+	}
+	system("pause");
+	return 0;
+	
+	
+	}
---
+++ Pares_1_a_100..cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stdio.h>
+
+int main(int argc, const char * argv[]){
+	
+	printf("mostrar numeros pares no intervalo de 1 a 100\n");
+	
+	int x=2;
+	while(x<=100)
+	{
+		
+		printf("%d \n", x);
+		x+=2;
+		
+	}
+	
+	
+	return 0;
+
+}
